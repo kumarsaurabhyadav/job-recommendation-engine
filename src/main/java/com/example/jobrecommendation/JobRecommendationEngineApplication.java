6638093c18ae9a2baeaeb0b3df993bce98f32c76
Kumar saurabh yadav <saurabhrahul101@gmail.com>
2026-09-15 16:33:59 +0530

    chore: initialize Spring Boot project

---
+++ src/main/java/com/example/jobrecommendation/JobRecommendationEngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobrecommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobRecommendationEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobRecommendationEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jobrecommendation/JobRecommendationEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobrecommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobRecommendationEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
